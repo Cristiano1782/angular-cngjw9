ea06dfa4621a618ea14c70dde724932468347bdb
Cristiano1782 <cristiano.panizza@marconirovereto.it>
2019-12-03 15:01:42 +0100

    1 file modified

--- src/app/libreria/libreria.component.ts
+++ src/app/libreria/libreria.component.ts
@@ -17,7 +17,7 @@ export class LibreriaComponent implements OnInit {
     this.libreria.push(new Libro('123-12-12345-12-1', 'Ma vala', 'Pietro Smusi', 2019));
     this.libreria.push(new Libro('999-12-12345-12-1', 'Russel crowe', 'Salmo Lebowski', 2017));
     this.libreria.push(new Libro('000-12-12345-12-1', 'sw1n60', 'th Supreme', 2019));
-    this.libreria.push(new Libro('777-12-12345-12-1', 'M1lan0', 'th Supreme', 2020));
+    this.libreria.push(new Libro('777-12-12345-12-1', 'M12ano', 'th Supreme', 2020));
   }
 
 }
