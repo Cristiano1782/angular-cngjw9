45f5e80c0c56656424ba1982d33679347e0cb59b
Cristiano1782 <cristiano.panizza@marconirovereto.it>
2019-12-03 14:36:19 +0100

    3 files modified

--- src/app/libreria/libreria.component.ts
+++ src/app/libreria/libreria.component.ts
@@ -15,7 +15,7 @@ export class LibreriaComponent implements OnInit {
   ngOnInit() {
     this.libreria = [];
     this.libreria.push(new Libro('123-12-12345-12-1', 'Ma vala', 'Pietro Smusi', 2019));
-    this.libreria.push(new Libro('999-12-12345-12-1', 'Russel crowe', 'Salmo Lebowski', 2019));
+    this.libreria.push(new Libro('999-12-12345-12-1', 'Russel crowe', 'Salmo Lebowski', 2017));
     this.libreria.push(new Libro('000-12-12345-12-1', 'sw1n60', 'th Supreme', 2019));
   }
 
